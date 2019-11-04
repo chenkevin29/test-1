55021125a8b952f275ae8d21508ad0c6aafc222c
chenkevin29 <57359422+chenkevin29@users.noreply.github.com>
2019-11-04 21:32:11 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Content from './components/content'
+
+function App() {
+  return (   
+   <div>
+     <Content />
+    
+   </div>
+  );
+}
+
+export default App;
---
+++ src/components/content.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import './content.css';
+
+class Content extends React.Component {
+
+    state = {
+        userName: "",
+
+    }
+
+    handleNameChage = (e) => {
+        this.setState({
+            userName: e.target.value
+        })
+    }
+
+    handleSubmit = () => {
+        if (this.state.userName == "") {
+            alert("kosong");
+            return
+        }else{
+            alert("nama = " + this.state.userName)
+            return
+        }
+    }
+
+    render () {
+        
+
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6 align-right">
+                         Name
+                    </div>
+                    <div className="col-md-6">
+                        <input
+                            type="text"
+                            value = {this.state.userName}
+                            onChange={this.handleNameChage}                         
+                        >
+                        </input>
+                    </div>
+                </div>
+                <div className="row align-center">
+                    <button
+                        onClick={this.handleSubmit}
+                    >
+                        Save
+                    </button>
+                </div>
+
+            </div>
+
+        )
+    }
+}
+export default Content
